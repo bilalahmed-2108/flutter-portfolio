4c06211ff00e916a885b0a0993e1cfe74021251f
sairakesh21 <sairakesh.saisrinivasan@gmail.com>
2025-09-29 17:33:36 +0530

    Deploy Flutter web build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1840582474"
+    serviceWorkerVersion: "3152557881"
   }
 });
